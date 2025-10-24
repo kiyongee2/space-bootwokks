65bb9eb1658ebc8db3a7e65ff3c152ddc41e45f3
kiyongee2 <kiyongee2@gmail.com>
2025-10-24 13:30:44 +0900

    friends member

--- Friends/src/main/java/com/springboot/controller/MemberController.java
+++ Friends/src/main/java/com/springboot/controller/MemberController.java
@@ -2,14 +2,30 @@ package com.springboot.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.springboot.dto.MemberDTO;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RequestMapping("/members")
 @Controller
 public class MemberController {
 
+	//회원 가입 페이지
 	@GetMapping("/join")
 	public String joinForm() {
 		return "member/join";
 	}
+	
+	//회원 가입 처리
+	@PostMapping("/join")
+	public String join(@ModelAttribute MemberDTO memberDTO) {
+		log.info("member: " + memberDTO);
+		return "redirect:/";
+	}
+	
 }
