2961e3d2a904c93d8c73b65789281407fe121fa9
kiyongee2 <kiyongee2@gmail.com>
2025-10-31 10:55:56 +0900

    validation

--- jwbook2/src/main/java/com/jwbook2/dto/Product.java
+++ jwbook2/src/main/java/com/jwbook2/dto/Product.java
@@ -7,10 +7,10 @@ import lombok.Data;
 @Data  //getter, setter, tostring 포함
 public class Product {
 	//제약 사항 메시지: 비어있을 수 없습니다.
-	@NotEmpty
+	@NotEmpty(message = "품명은 필수 항목입니다.")
 	private String name; //상품명
 	
 	//0 이상이어야 합니다.
-	@Min(value=0)
-	private Integer price;   //가격
+	@Min(value=0, message = "가격은 0원 이상이어야 합니다.")
+	private int price;   //가격
 }
